c5055b6171909196fd3cac5b78140eee20b66a7a
momo1108 <38344292+momo1108@users.noreply.github.com>
2019-07-28 23:44:18 +0900

    teamproject

--- 이론 수업/React/teampro/src/Components/Body/Detailsrch.js
+++ 이론 수업/React/teampro/src/Components/Body/Detailsrch.js
@@ -8,58 +8,67 @@ class Detailsrch extends Component {
 
     render() {
         var localselect = (e) => {
+            // togglebtn[0].children[0].checked
             let selector = document.getElementsByClassName('locals');
+            let selector_local = document.getElementsByClassName('localdiv');
             let i = 0;
+            for (let j=0; j<selector_local.length; j++) {
+                selector_local[j].style.display = 'none';
+              }
+            let boxclass = e.target.getAttribute('name');
+            document.getElementsByClassName(boxclass)[0].style.display = 'block';
             while (selector[i]) {
-                selector[i].style.backgroundColor = 'white';
-                selector[i].style.fontWeight = 'normal';
+                selector[i].className = 'locals';
                 i++;
             }
-            e.target.style.backgroundColor = 'rgb(227, 242, 255)';
-            e.target.style.fontWeight = 'bold';
+            if(e.target.className=='localtext'){
+                e.target.parentElement.className = 'locals localclick';
+            } else {
+                e.target.className = 'locals localclick';
+            }
         }
         return (
             <Jumbotron className='searchjumbo'>
                 <p>지역</p>
                 <ul>
-                    <NavLink exact to='/workerinfo/'><li className='locals' onClick={localselect}>전체</li></NavLink>
-                    <NavLink to='/workerinfo/seoul'><li className='locals' onClick={localselect}>서울</li></NavLink>
-                    <NavLink to='/workerinfo/gyeongi'><li className='locals' onClick={localselect}>경기</li></NavLink>
-                    <NavLink to='/workerinfo/incheon'><li className='locals' onClick={localselect}>인천</li></NavLink>
-                    <NavLink to='/workerinfo/busan'><li className='locals' onClick={localselect}>부산</li></NavLink>
-                    <NavLink to='/workerinfo/daegue'><li className='locals' onClick={localselect}>대구</li></NavLink>
-                    <NavLink to='/workerinfo/daejeon'><li className='locals' onClick={localselect}>대전</li></NavLink>
-                    <NavLink to='/workerinfo/sejong'><li className='locals' onClick={localselect}>세종</li></NavLink>
-                    <NavLink to='/workerinfo/gwangju'><li className='locals' onClick={localselect}>광주</li></NavLink>
-                    <NavLink to='/workerinfo/ulsan'><li className='locals' onClick={localselect}>울산</li></NavLink>
-                    <NavLink to='/workerinfo/gangwon'><li className='locals' onClick={localselect}>강원</li></NavLink>
-                    <NavLink to='/workerinfo/gyungnam'><li className='locals' onClick={localselect}>경남</li></NavLink>
-                    <NavLink to='/workerinfo/gyungbuk'><li className='locals' onClick={localselect}>경북</li></NavLink>
-                    <NavLink to='/workerinfo/jeonnam'><li className='locals' onClick={localselect}>전남</li></NavLink>
-                    <NavLink to='/workerinfo/jeonbuk'><li className='locals' onClick={localselect}>전북</li></NavLink>
-                    <NavLink to='/workerinfo/choongnam'><li className='locals' onClick={localselect}>충남</li></NavLink>
-                    <NavLink to='/workerinfo/choongbuk'><li className='locals' onClick={localselect}>충북</li></NavLink>
-                    <NavLink to='/workerinfo/jeju'><li className='locals' onClick={localselect}>제주</li></NavLink>
-                    <NavLink to='/workerinfo/othercountry'><li className='locals' onClick={localselect}>해외</li></NavLink>
+                    <li className='locals' onClick={localselect}><span className='localtext'>전체</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv1'><span className='localtext' name='localdiv1'>서울</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv2'><span className='localtext' name='localdiv2'>경기</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv3'><span className='localtext' name='localdiv3'>인천</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv4'><span className='localtext' name='localdiv4'>부산</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv5'><span className='localtext' name='localdiv5'>대구</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv6'><span className='localtext' name='localdiv6'>대전</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv7'><span className='localtext' name='localdiv7'>세종</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv8'><span className='localtext' name='localdiv8'>광주</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv9'><span className='localtext' name='localdiv9'>울산</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv10'><span className='localtext' name='localdiv10'>강원</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv11'><span className='localtext' name='localdiv11'>경남</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv12'><span className='localtext' name='localdiv12'>경북</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv13'><span className='localtext' name='localdiv13'>전남</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv14'><span className='localtext' name='localdiv14'>전북</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv15'><span className='localtext' name='localdiv15'>충남</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv16'><span className='localtext' name='localdiv16'>충북</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv17'><span className='localtext' name='localdiv17'>제주</span></li>
+                    <li className='locals' onClick={localselect} name='localdiv18'><span className='localtext' name='localdiv18'>해외</span></li>
                 </ul>
-                <Route path='/workerinfo/seoul' component={Seoul} />
-                <Route path='/workerinfo/gyeongi' component={Gyeongi} />
-                <Route path='/workerinfo/incheon' component={Incheon} />
-                <Route path='/workerinfo/busan' component={Busan} />
-                <Route path='/workerinfo/daegue' component={Daegue} />
-                <Route path='/workerinfo/daejeon' component={Daejeon} />
-                <Route path='/workerinfo/sejong' component={Sejong} />
-                <Route path='/workerinfo/gwangju' component={Gwangju} />
-                <Route path='/workerinfo/ulsan' component={Ulsan} />
-                <Route path='/workerinfo/gangwon' component={Gangwon} />
-                <Route path='/workerinfo/gyungnam' component={Gyungnam} />
-                <Route path='/workerinfo/gyungbuk' component={Gyungbuk} />
-                <Route path='/workerinfo/jeonnam' component={Jeonnam} />
-                <Route path='/workerinfo/jeonbuk' component={Jeonbuk} />
-                <Route path='/workerinfo/choongnam' component={Choongnam} />
-                <Route path='/workerinfo/choongbuk' component={Choongbuk} />
-                <Route path='/workerinfo/jeju' component={Jeju} />
-                <Route path='/workerinfo/othercountry' component={Othercountry} />
+                <Seoul/>
+                <Gyeongi/>
+                <Incheon/>
+                <Busan/>
+                <Daegue/>
+                <Daejeon/>
+                <Sejong/>
+                <Gwangju/>
+                <Ulsan/>
+                <Gangwon/>
+                <Gyungnam/>
+                <Gyungbuk/>
+                <Jeonnam/>
+                <Jeonbuk/>
+                <Choongnam/>
+                <Choongbuk/>
+                <Jeju/>
+                <Othercountry/>
                 <hr className='firsthr' />
                 <p>형태</p><hr />
 
